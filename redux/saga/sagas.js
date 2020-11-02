25b282bf39803e295f741452cdbbd482fe6541a9
Maks <blackrock.09.04@gmail.com>
2020-11-02 19:22:22 +0300

    fix bag

--- redux/saga/sagas.js
+++ redux/saga/sagas.js
@@ -6,7 +6,6 @@ let date = new Date();
 let since = date.toISOString().slice(0, 10);
 date = new Date(date.getTime() + 1000 * 60 * 60 * 24 * 10)
 let till = date.toISOString().slice(0, 10);
-
 const fetchSkyScanner = async () => {
   const resp = await fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/RUB/en-US/SFO-sky/ORD-sky/${since}?inboundpartialdate=${till}`, {
     "method": "GET",
--- scripts/getDate.js
+++ scripts/getDate.js
@@ -1,4 +1,4 @@
 export function getDate(date) {
   var strArray = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  return `${date.getDay()} ${strArray[date.getMonth()]} ${date.getFullYear()}`
+  return `${date.getDate()} ${strArray[date.getMonth()]} ${date.getFullYear()}`
 }
